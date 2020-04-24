963008ad5812f37df0cf0ef2316b1a9007edd874
Jessica <github email address>
2020-04-24 15:47:55 -0400

    First set

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Backer
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+    
+    def backed_projects
+        ProjectBacker.map do |project|
+            project.backer == self
+        end
+    end
+end
