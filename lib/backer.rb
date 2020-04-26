dce4184caab529c040e1f13d1a0949724f4a96d0
josephchilin <joseph@josephchilin.com>
2020-04-26 18:50:28 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Backer
     attr_accessor :name
     @@all = []
@@ -12,10 +14,35 @@ class Backer
         ProjectBacker.new(project, self)
     end
     def backed_projects
-        project_backers = ProjectBacker.all.select {|project| project.backer == self}
-        project_backers.map {|backed_project| backed_project.project}
+        project_backers = ProjectBacker.all.select do |project| 
+            project.backer == self
+            # binding.pry
+
+    # returns [3] pry(#<Backer>)> project_backers
+    # => [#<ProjectBacker:0x00007fc0b22b29e0
+    #   @backer=#<Backer:0x00007fc0b22b2a08 @name="Meryl">,
+    #   @project=#<Project:0x00007fc0b22b2a80 @title="Karaoke">,
+    #   @title=nil>,
+    #  #<ProjectBacker:0x00007fc0b22b2918
+    #   @backer=#<Backer:0x00007fc0b22b2a08 @name="Meryl">,
+    #   @project=#<Project:0x00007fc0b22b2af8 @title="Ruby, Ruby, and More Ruby">,
+    #   @title=nil>]
+
+    # project_backers returns an array with every ProjectBacker data instance that == self
+    
+        end
+
+        project_backers.map do |backed_project| 
+            backed_project.project
+
+    # project_backers.map filters through the large dataset of project_backers so only the 
+    # .project variables are returned
+            # binding.pry
+        end
+        # binding.pry
         #returns an array of projects that are associated with this instance
         # 1) Backer - backed_projects returns an array of projects associated with this Backer instance
         # Failure/Error: expect(meryl.backed_projects).to eq([karaoke, book])
     end
+    # binding.pry
 end 
